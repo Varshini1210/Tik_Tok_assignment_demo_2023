4a5863a8d25b8409844330510157f53c2a8ba87b
Varshini <varshinip002@gmail.com>
2023-06-14 23:53:23 +0800

    commit

--- rpc-server/handler.go
+++ rpc-server/handler.go
@@ -7,25 +7,112 @@ import (
 	"github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc"
 )
 
+
 // IMServiceImpl implements the last service interface defined in the IDL.
 type IMServiceImpl struct{}
 
 func (s *IMServiceImpl) Send(ctx context.Context, req *rpc.SendRequest) (*rpc.SendResponse, error) {
-	resp := rpc.NewSendResponse()
-	resp.Code, resp.Msg = areYouLucky()
+	// Save the message details to the database
+	err := saveMessageToDatabase(req.Chat, req.Text, req.Sender)
+	if err != nil {
+		return nil, err
+	}
+	// Create and return the send response
+	resp := &api.SendResponse{}
 	return resp, nil
+
 }
+
 
 func (s *IMServiceImpl) Pull(ctx context.Context, req *rpc.PullRequest) (*rpc.PullResponse, error) {
-	resp := rpc.NewPullResponse()
-	resp.Code, resp.Msg = areYouLucky()
+	// Retrieve messages from the database based on the pull request
+	messages, err := retrieveMessagesFromDatabase(req.Chat, req.Cursor, req.Limit, req.Reverse)
+	if err != nil {
+		return nil, err
+	}
+	// Create the pull response with the retrieved messages
+	resp := &api.PullResponse{
+		Messages: messages,
+		HasMore:  len(messages) == req.Limit, // Determine if there are more messages available
+	}
+	// Set the next cursor if there are more messages
+	if len(messages) > 0 {
+		lastMessage := messages[len(messages)-1]
+		resp.NextCursor = lastMessage.SendTime
+	}
+
 	return resp, nil
 }
 
-func areYouLucky() (int32, string) {
-	if rand.Int31n(2) == 1 {
-		return 0, "success"
-	} else {
-		return 500, "oops"
+
+
+func saveMessageToDatabase(db *sql.DB, sender, receiver, message string) error {
+	query := "INSERT INTO user (sender, receiver, message, timestamp) VALUES (?, ?, ?, CURRENT_TIMESTAMP)"
+	_, err := db.Exec(query, sender, receiver, message)
+	if err != nil {
+		return err
 	}
+	log.Println("Message saved to the database")
+	return nil
+}
+
+
+
+func saveMessageToDatabase(db *sql.DB, sender, receiver, message string) error {
+	query := "INSERT INTO user (sender, receiver, message, timestamp) VALUES (?, ?, ?, CURRENT_TIMESTAMP)"
+	_, err := db.Exec(query, sender, receiver, message)
+	if err != nil {
+		return err
+	}
+	log.Println("Message saved to the database")
+	return nil
+}
+
+func retrieveMessagesFromDatabase(db *sql.DB, chat string, cursor int64, limit int32, reverse bool) ([]*api.Message, error) {
+	var query string
+	var args []interface{}
+
+	if reverse {
+		query = "SELECT sender, receiver, message, timestamp FROM user WHERE (sender = ? AND receiver = ?) OR (sender = ? AND receiver = ?) AND timestamp < ? ORDER BY timestamp DESC LIMIT ?"
+		args = []interface{}{chat, chat, chat, chat, cursor, limit}
+	} else {
+		query = "SELECT sender, receiver, message, timestamp FROM user WHERE (sender = ? AND receiver = ?) OR (sender = ? AND receiver = ?) AND timestamp > ? ORDER BY timestamp ASC LIMIT ?"
+		args = []interface{}{chat, chat, chat, chat, cursor, limit}
+	}
+
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query(args...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	messages := []*api.Message{}
+	for rows.Next() {
+		var sender, receiver, message string
+		var timestamp string
+		err := rows.Scan(&sender, &receiver, &message, &timestamp)
+		if err != nil {
+			return nil, err
+		}
+		message := &api.Message{
+			Sender:    sender,
+			Receiver:  receiver,
+			Message:   message,
+			Timestamp: timestamp,
+		}
+		messages = append(messages, message)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	log.Printf("Retrieved %d messages from the database", len(messages))
+	return messages, nil
 }
--- rpc-server/main.go
+++ rpc-server/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"database/sql"
+    _ "github.com/go-sql-driver/mysql"
 
 	rpc "github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc/imservice"
 	"github.com/cloudwego/kitex/pkg/rpcinfo"
@@ -10,6 +12,13 @@ import (
 )
 
 func main() {
+	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/TikTok_Assignment")
+    if err != nil {
+        // Handle the error
+        log.Fatal(err)
+    }
+    defer db.Close()
+
 	r, err := etcd.NewEtcdRegistry([]string{"etcd:2379"}) // r should not be reused.
 	if err != nil {
 		log.Fatal(err)
@@ -23,4 +32,24 @@ func main() {
 	if err != nil {
 		log.Println(err.Error())
 	}
+}
+
+
+
+func saveMessage(db *sql.DB, senderName, recipientName, message string) error {
+	// Prepare the SQL statement
+	stmt, err := db.Prepare("INSERT INTO user (sender, receiver, message, timestamp) VALUES (?, ?, ?, CURRENT_TIMESTAMP)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	// Execute the prepared statement with the values
+	_, err = stmt.Exec(senderName, recipientName, message)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Message saved successfully!")
+	return nil
 }
